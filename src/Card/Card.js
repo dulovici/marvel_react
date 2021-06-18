a80d624476c9b0b563db8a9869ab412335129c83
BIT Student <igorx24@gmail.com>
2021-06-18 12:26:53 +0200

    cleanup

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 export const Card = (props) => {
     const { data } = props;
     const { id } = props;
-    const {addMember} = props;
-    
-    
+    const { addMember } = props;
+
+
     return (
         <div className='card'>
             <h2>{data.name}</h2>
             <img src={`${data.thumbnail.path}.${data.thumbnail.extension}`} alt='img' />
             <div className='btns'>
+
                 <Link to={`/single/${id}`}>
                     <button>Info</button>
                 </Link>
 
-                <button onClick={()=>addMember(id)}>Add</button>
+                <button onClick={() => addMember(id)}>Add</button>
             </div>
         </div>
     )
--- src/Single/Single.js
+++ src/Single/Single.js
@@ -13,33 +13,33 @@ export const Single = (props) => {
         fetch(`http://gateway.marvel.com/v1/public/characters/${id}?apikey=${key}`)
             .then(res => res.json())
             .then(dta => setSingleData(dta.data.results[0]))
-    }, [])
+    }, [id])
 
     console.log(singleData);
 
     return (
-        singleData.length !== 0 
-        ? 
-        <div className='single-wr'>
-            <div className='single-hdr'>
-            <h2>{singleData.name}</h2>
-                <Link to='/'>
-                    <p>Homepage</p>
-                </Link>
+        singleData.length !== 0
+            ?
+            <div className='single-wr'>
+                <div className='single-hdr'>
+                    <h2>{singleData.name}</h2>
+                    <Link to='/'>
+                        <p>Homepage</p>
+                    </Link>
+                </div>
+
+                <div className='single-main'>
+
+                    <img src={`${singleData?.thumbnail.path}.${singleData?.thumbnail.extension}`} alt='img' />
+                    <h2>Description:</h2>
+
+                    {singleData?.description.length === 0
+                        ? <h1>Sorry, no description :(</h1>
+                        : <p>{singleData.description}</p>}
+
+                </div>
             </div>
+            : <Loader />
 
-            <div className='single-main'>
-
-                <img src={`${singleData?.thumbnail.path}.${singleData?.thumbnail.extension}`} alt='img' />
-                <h2>Description:</h2>
-
-                {singleData?.description.length === 0 
-                ?<h1>Sorry, no description :(</h1>
-                :<p>{singleData.description}</p>}
-
-            </div>
-    </div>
-    : <Loader />
-    
     )
 }
